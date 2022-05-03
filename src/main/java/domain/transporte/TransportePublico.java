eda32edcfe47ea5734e94b7c5e98976e4c76bf8b
IgnacioArdanaz <nachitoard@gmail.com>
2022-05-03 13:23:48 -0300

    Actualizo diagrama y elimino paradas inicial y final

--- src/main/java/domain/transporte/TransportePublico.java
+++ src/main/java/domain/transporte/TransportePublico.java
@@ -10,18 +10,10 @@ public class TransportePublico extends Transporte {
   private TipoDeTransportePublico tipoDeTransportePublico;
   private List<Ubicacion> paradas = new ArrayList<>();
   private String linea;
-  //private Ubicacion paradaInicio; // Agrego paradaInicio y paradaFin como atributos
-  //private Ubicacion paradaFin;
 
-  //public TransportePublico(TipoDeTransportePublico tipoDeTransportePublico, String linea,
-  // Ubicacion paradaInicio, Ubicacion paradaFin) {
   public TransportePublico(TipoDeTransportePublico tipoDeTransportePublico, String linea) {
     this.tipoDeTransportePublico = tipoDeTransportePublico;
     this.linea = linea;
-    //this.paradaInicio = paradaInicio;//Agrego paradaInicio y paradaFin como parametros
-    // necesarios para el constructor.
-    //this.paradaFin = paradaFin;No creo que sea necesario teniendo una lista de paradas,suponiendo
-    //que la primera sera el elemento 0 (se puede conseguir con .get(i)) y lo mismo para la ultima
     setTipoTransporte(TipoTransporte.PUBLICO);
   }
 
@@ -37,14 +29,6 @@ public class TransportePublico extends Transporte {
     return linea;
   }
 
-  /*public Ubicacion getParadaInicio() {
-    return paradaInicio;
-  }
-
-  public Ubicacion getParadaFin() {
-    return paradaFin;
-  }
-*/
   public void addParadas(Ubicacion... ubicaciones) {
     Collections.addAll(this.paradas, ubicaciones);
   }
