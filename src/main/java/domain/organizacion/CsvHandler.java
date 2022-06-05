b3f0d221c57777b76e937e586209d17dd5942202
Matias Beorlegui <mbeorlegui@frba.utn.edu.ar>
2022-06-05 17:59:52 -0300

    Agrego parseo de csv

--- src/main/java/domain/organizacion/CsvHandler.java
+++ src/main/java/domain/organizacion/CsvHandler.java
@@ -1,9 +1,45 @@
 package domain.organizacion;
 
-public class CsvHandler {
+import com.opencsv.CSVReader;
+import com.opencsv.bean.CsvToBeanBuilder;
+import com.opencsv.exceptions.CsvValidationException;
+import domain.medicion.Medicion;
+import domain.medicion.MedicionRead;
 
-  void agregarmedicion() {
-    // TODO:
-    // Deberia agregar al archivo .csv la medicion en cuestion
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+public class CsvHandler {
+  static final String FILE_PATH = "data/Mediciones.csv";
+
+  private List<Medicion> getMediciones() {
+    // TODO: Completar definicion
+    return null;
+  }
+
+  private Medicion leerMedicion() {
+    return null;
+  }
+
+  private List<MedicionRead> getMedicionesRead(String FILE_PATH) throws IOException {
+    CSVReader csvReader = openFile(FILE_PATH);
+    return getParse(csvReader);
+  }
+
+  private List<MedicionRead> getParse(CSVReader csvReader) {
+    return new CsvToBeanBuilder<MedicionRead>(csvReader)
+        .withType(MedicionRead.class)
+        .withSeparator(',')
+        .withIgnoreLeadingWhiteSpace(true)
+        .withIgnoreEmptyLine(true)
+        .build()
+        .parse();
+  }
+
+  private CSVReader openFile(String FILE_PATH) throws FileNotFoundException {
+    FileReader file = new FileReader(FILE_PATH);
+    return new CSVReader(file);
   }
 }
