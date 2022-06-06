39072d33d8a743779102e4bf3100b32887256505
Matias Beorlegui <mbeorlegui@frba.utn.edu.ar>
2022-06-06 18:25:32 -0300

    Rediseño de TipoConsumo

--- src/main/java/domain/medicion/TipoConsumo.java
+++ src/main/java/domain/medicion/TipoConsumo.java
@@ -1,8 +1,18 @@
 package domain.medicion;
 
-public class TipoConsumo {
-  private String nombre;
-  private Unidad unidad;
-  private Actividad actividad;
-  private Alcance alcance;
+public enum TipoConsumo {
+  GAS_NATURAL,
+  DIESEL_GASOIL,
+  NAFTA,
+  CARBON,
+  CC_GASOIL,
+  CC_NAFTA,
+  ELECTRICIDAD,
+  MEDIO_DE_TRANSPORTE,
+  DISTANCIA_MEDIA_RECORRIDA,
 }
+
+// TODO: Analizar refactor futuro para realizar el calculo de huella de carbono
+//  utilizando varias unidades. Quizas convenga utilizar un strategy y que cada
+//  medicion sepa su consumo en una unidad determinada
+
