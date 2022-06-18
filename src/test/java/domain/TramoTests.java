608d217288d5b3bb8db8fc5fd835aa2d95ed5ed4
alejosandrini <alejo.sandrini@gmail.com>
2022-06-18 12:22:09 -0300

    Agrego la clase ResultadoDeDistancia a los tests de tramo

--- src/test/java/domain/TramoTests.java
+++ src/test/java/domain/TramoTests.java
@@ -1,6 +1,8 @@
 package domain;
 
+import domain.medicion.Unidad;
 import domain.services.apidistancias.CalculadoraDeDistancia;
+import domain.services.apidistancias.entities.ResultadoDistancia;
 import domain.trayecto.Tramo;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -11,11 +13,13 @@ import static org.mockito.Mockito.*;
 
 public class TramoTests {
   private Tramo casaHastaLinea7;
+  private ResultadoDistancia resultadoDistancia;
 
   @BeforeEach
   void init() {
     InicializacionTests inicializador = new InicializacionTests();
     casaHastaLinea7 = inicializador.casaHastaLinea7();
+    resultadoDistancia = new ResultadoDistancia(50, "km");
   }
 
   @DisplayName("Calculo de distancia intermedia de un tramo")
@@ -24,8 +28,8 @@ public class TramoTests {
     CalculadoraDeDistancia calculadoraMock = mock(CalculadoraDeDistancia.class);
     casaHastaLinea7.setCalculadoraDeDistancia(calculadoraMock);
     when(calculadoraMock.distancia(casaHastaLinea7.getOrigenDeTramo(), casaHastaLinea7.getDestinoDeTramo()))
-        .thenReturn(50.0);
-
-    assertEquals(50, casaHastaLinea7.distanciaIntermedia());
+        .thenReturn(resultadoDistancia.getValor());
+    assertEquals(50,resultadoDistancia.getValor());
+    assertEquals(resultadoDistancia.getValor(), casaHastaLinea7.distanciaIntermedia());
   }
 }
