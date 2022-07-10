92d57d3b9c691f978ad057acca5085de214823b4
AlejoGol <alejogol02@gmail.com>
2022-07-10 11:50:30 -0300

    Agregado: Huella de carbono de sector territorial

--- src/main/java/domain/organizacion/SectorTerritorial.java
+++ src/main/java/domain/organizacion/SectorTerritorial.java
@@ -1,8 +1,17 @@
 package domain.organizacion;
 
+import domain.medicion.Periodicidad;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class SectorTerritorial {
   private List<Organizacion> organizaciones = new ArrayList<>();
+
+  public double huellaDeCarbonoEnPeriodo(Periodicidad periodicidad, String periodoDeImputacion) {
+    return organizaciones
+        .stream()
+        .mapToDouble(org -> org.huellaDeCarbonoEnPeriodo(periodicidad, periodoDeImputacion))
+        .sum();
+  }
 }
