f83f2f623a60a8ec52a7e70e1fc0d88620ec9dc0
Matias Beorlegui <mbeorlegui@frba.utn.edu.ar>
2022-09-13 18:35:38 -0300

    Agrega nuevas clases para persistir al xml

--- src/main/java/domain/dbrunner/Runner.java
+++ src/main/java/domain/dbrunner/Runner.java
@@ -7,13 +7,15 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
 
 public class Runner {
+  // Probar con: mvn compile exec:java
   public static void main(String[] args) {
-    System.out.println("Hola!");
+    System.out.println("Ejecutando queries!");
     EntityManager em = PerThreadEntityManagers.getEntityManager();
     EntityTransaction et = em.getTransaction();
     et.begin();
     Ubicacion ubicacion = new Ubicacion(1, "Calle Falsa", "123");
     em.persist(ubicacion);
     et.commit();
+    System.out.println("Cerrando conexion");
   }
 }
