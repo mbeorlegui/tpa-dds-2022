4cf5340d6d19653e09f266cc055f71dd4518eae1
AlejoGol <alejogol02@gmail.com>
2022-09-21 11:33:49 -0300

    Agregado: Reportes de composicion de hc

--- src/main/java/domain/reports/ReportGenerator.java
+++ src/main/java/domain/reports/ReportGenerator.java
@@ -23,7 +23,7 @@ public class ReportGenerator implements WithGlobalEntityManager {
   }
 
   @SuppressWarnings("unchecked")
-  public static List<Organizacion> getOrganizacionesPorSector(SectorTerritorial sector) {
+  public List<Organizacion> getOrganizacionesPorSector(SectorTerritorial sector) {
     return em
         .createQuery("from Organizacion where sector_territorial_id = :sector")
         .setParameter("sector", sector)
@@ -78,5 +78,51 @@ public class ReportGenerator implements WithGlobalEntityManager {
   public double evolucionDeHcEnOrganizacion(
       Organizacion organizacion, String periodoDeInicio, String periodoDeFin) {
     return 0;
+  }
+
+
+  public ReporteDeComposicion composicionHcDeSectorTerritorial(SectorTerritorial sectorTerritorial,
+                                                               Periodicidad periodicidad,
+                                                               String periodoDeImputacion,
+                                                               UnidadEquivalenteCarbono unidadDeseada) {
+    List<Organizacion> organizaciones = this.getOrganizacionesPorSector(sectorTerritorial);
+    return this.composicionHcDeOrganizaciones(organizaciones, periodicidad, periodoDeImputacion, unidadDeseada);
+  }
+
+  public ReporteDeComposicion composicionHcDeOrganizacion(Organizacion organizacion,
+                                                          Periodicidad periodicidad,
+                                                          String periodoDeImputacion,
+                                                          UnidadEquivalenteCarbono unidadDeseada) {
+    return new ReporteDeComposicion(
+        organizacion.hcMedicionesEnPeriodo(periodicidad, periodoDeImputacion, unidadDeseada),
+        organizacion.hcTrayectosMiembros(periodicidad, unidadDeseada);
+  }
+
+  private ReporteDeComposicion composicionHcDeOrganizaciones(List<Organizacion> organizaciones,
+                                                            Periodicidad periodicidad,
+                                                            String periodoDeImputacion,
+                                                            UnidadEquivalenteCarbono unidadDeseada) {
+    return new ReporteDeComposicion(
+        this.hcMediciones(organizaciones, periodicidad, periodoDeImputacion, unidadDeseada),
+        this.hcTrayectos(organizaciones, periodicidad, unidadDeseada));
+  }
+
+  private double hcMediciones(List<Organizacion> organizaciones,
+                              Periodicidad periodicidad,
+                              String periodoDeImputacion,
+                              UnidadEquivalenteCarbono unidadDeseada) {
+    return organizaciones
+        .stream()
+        .mapToDouble(org -> org.hcMedicionesEnPeriodo(periodicidad, periodoDeImputacion, unidadDeseada))
+        .sum();
+  }
+
+  private double hcTrayectos(List<Organizacion> organizaciones,
+                             Periodicidad periodicidad,
+                             UnidadEquivalenteCarbono unidadDeseada) {
+    return organizaciones
+        .stream()
+        .mapToDouble(org -> org.hcTrayectosMiembros(periodicidad, unidadDeseada))
+        .sum();
   }
 }
