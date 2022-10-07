4d82378e8d8868bb280764fab7442a17fba210d7
Matias Beorlegui <mbeorlegui@frba.utn.edu.ar>
2022-10-07 16:00:46 -0300

    Resuelve conflictos en RepoTiposConsumos

--- src/main/java/domain/medicion/RepoTiposConsumos.java
+++ src/main/java/domain/medicion/RepoTiposConsumos.java
@@ -16,54 +16,16 @@ public class RepoTiposConsumos {
   private EntityManager em = PerThreadEntityManagers.getEntityManager();
 
   public List<TipoConsumo> consumos = new ArrayList<>();
-<<<<<<< HEAD
-  private TipoConsumo gasNatural = new TipoConsumo(Actividad.COMBUSTION_FIJA,
-      Alcance.EMISION_DIRECTA, Unidad.M3, new FactorDeEmision(0.2, UnidadEquivalenteCarbono.GRAMO),
-      "GAS_NATURAL");
-  private TipoConsumo dieselGasoil = new TipoConsumo(Actividad.COMBUSTION_FIJA,
-      Alcance.EMISION_DIRECTA, Unidad.LT, new FactorDeEmision(2.5, UnidadEquivalenteCarbono.GRAMO),
-      "DIESEL_GASOIL");
-  private TipoConsumo nafta = new TipoConsumo(Actividad.COMBUSTION_FIJA,
-      Alcance.EMISION_DIRECTA, Unidad.LT, new FactorDeEmision(2.3, UnidadEquivalenteCarbono.GRAMO),
-      "NAFTA");
-  private TipoConsumo carbon = new TipoConsumo(Actividad.COMBUSTION_FIJA,
-      Alcance.EMISION_DIRECTA, Unidad.KG, new FactorDeEmision(1.6, UnidadEquivalenteCarbono.GRAMO),
-      "CARBON");
-  private TipoConsumo ccGasoil = new TipoConsumo(Actividad.COMBUSTION_MOVIL,
-      Alcance.EMISION_DIRECTA, Unidad.LT, new FactorDeEmision(2.7, UnidadEquivalenteCarbono.GRAMO),
-      "CC_GASOIL");
-  private TipoConsumo ccNafta = new TipoConsumo(Actividad.COMBUSTION_MOVIL,
-      Alcance.EMISION_DIRECTA, Unidad.LT, new FactorDeEmision(2.4, UnidadEquivalenteCarbono.GRAMO),
-      "CC_NAFTA");
-  private TipoConsumo electricidad = new TipoConsumo(Actividad.ELECTRICIDAD,
-      Alcance.EMISION_INDIRECTA, Unidad.KWH, new FactorDeEmision(0.4, UnidadEquivalenteCarbono.GRAMO),
-      "ELECTRICIDAD");
-  private TipoConsumo medioDeTransporte = new TipoConsumo(Actividad.LOGISTICA,
-      Alcance.OTRAS_EMISIONES, Unidad.SIN_UNIDAD,
-      new FactorDeEmision(0.1, UnidadEquivalenteCarbono.GRAMO),
-      "MEDIO_DE_TRANSPORTE");
-  private TipoConsumo distanciaMedia = new TipoConsumo(Actividad.LOGISTICA,
-      Alcance.OTRAS_EMISIONES, Unidad.KM, new FactorDeEmision(1.0, UnidadEquivalenteCarbono.GRAMO),
-      "DISTANCIA_MEDIA_RECORRIDA");
-=======
->>>>>>> a6a37aa4c7a9bd8f1a85beaad12c4ad60bace37d
 
   private static final RepoTiposConsumos INSTANCE =
       new RepoTiposConsumos();
 
   private RepoTiposConsumos() {}
 
-<<<<<<< HEAD
-  private RepoTiposConsumos() {
-    agregarTiposDeConsumo(gasNatural, dieselGasoil, nafta,
-        carbon, ccGasoil, ccNafta, electricidad,
-        medioDeTransporte, distanciaMedia);
-=======
   public List<TipoConsumo> getTiposConsumos() {
     return em
         .createQuery("from TipoConsumo")
         .getResultList();
->>>>>>> a6a37aa4c7a9bd8f1a85beaad12c4ad60bace37d
   }
 
   public void agregarTiposDeConsumo(TipoConsumo... tiposConsumo) {
