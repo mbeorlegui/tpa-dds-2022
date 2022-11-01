8a9f09717648b42937e05b4a3282dd96bdde5221
alejosandrini <alejo.sandrini@gmail.com>
2022-10-31 23:41:43 -0300

    Modifico palabras en ingles, pantalla de medicion particular y agrego funciones

--- src/main/java/domain/controllers/RegistrarMedicionController.java
+++ src/main/java/domain/controllers/RegistrarMedicionController.java
@@ -5,7 +5,11 @@ import spark.Request;
 import spark.Response;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+
+import domain.medicion.RepoTiposConsumos;
+import domain.medicion.TipoConsumo;
 
 public class RegistrarMedicionController {
   public ModelAndView registrarMedicionCsv(Request request, Response response) {
@@ -17,6 +21,8 @@ public class RegistrarMedicionController {
   public ModelAndView registrarMedicionParticular(Request request, Response response) {
     Map<String, Object> model = new HashMap<>();
     model.put("usuario_logueado", request.session().attribute("usuario_logueado"));
+    List<TipoConsumo> tipos_consumo = RepoTiposConsumos.getInstance().getTiposConsumos();
+    model.put("tipos_consumo", tipos_consumo);
     return new ModelAndView(model, "registrarMedicionParticular.hbs");
   }
 }
--- src/main/resources/public/js/main.js
+++ src/main/resources/public/js/main.js
@@ -165,3 +165,34 @@ function cambiarSeleccionReporte(reporteActual){
     console.log(reporteActual)
     console.log(reporteAnterior)
 }
+
+/*
+    Pantalla: registrarMedicionParticular
+    Funcion: cambiar title icono info de periodo segun la periodicidad
+*/
+function obtenerFormato(icono){
+    let inputPeriodicidad = document.getElementById("periodicidad").value
+    if(inputPeriodicidad==""){
+        icono.setAttribute("title","Seleccione primero la periodicidad")
+    }else if(inputPeriodicidad=="Mensual"){
+        icono.setAttribute("title","Inserte periodo con formato: 01/2022")
+    }else if(inputPeriodicidad=="Anual"){
+        icono.setAttribute("title","Inserte periodo con formato: 2022")
+    }else{
+        icono.setAttribute("title","Seleccione una periodicidad valida")
+    }
+}
+
+/*
+    Pantalla: registrarMedicionParticular
+    Funcion: habilitar input periodo luego de seleccionar periodicidad
+*/
+function habilitarPeriodo(selectPeriodo){
+    let periodicidad = selectPeriodo.value
+    let inputPeriodo = document.getElementById("periodoImputacion")
+    if (periodicidad=="Mensual" || periodicidad=="Anual"){
+        inputPeriodo.removeAttribute("disabled")
+    } else {
+        inputPeriodo.removeAttribute("disabled")
+    }
+}
