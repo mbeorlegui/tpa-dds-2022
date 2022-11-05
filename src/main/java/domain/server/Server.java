bebd0750fd669b5f6d0a7ff84d76da966b9bc9b7
Matias Beorlegui <mbeorlegui@frba.utn.edu.ar>
2022-11-05 02:45:08 -0300

    Toma el puerto del server de variable de entorno, pero si no esta definido toma 9000

--- src/main/java/domain/server/Server.java
+++ src/main/java/domain/server/Server.java
@@ -6,9 +6,11 @@ import spark.Spark;
 public class Server {
   public static void main(String[] args) {
     Bootstrap.init();
-    Spark.port(9000);
-    // TODO: Cambiar puerto por variable de entorno para deploy
-    // Spark.port(Integer.parseInt(System.getenv("PORT")));
+    if(System.getenv("PORT") != null) {
+      Spark.port(Integer.parseInt(System.getenv("PORT")));
+    } else {
+      Spark.port(9000);
+    }
     Router.configure();
   }
 }
